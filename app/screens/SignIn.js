d5c6577623dd495623794bc545ba3c4f801aca27
Timo Mätäsaho <timo.matasaho@gmail.com>
2019-10-14 22:16:03 +0300

    More progress on the sign in screen

--- app/screens/SignIn.js
+++ app/screens/SignIn.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-import { Card, Input } from 'react-native-elements';
+import { PropTypes } from 'prop-types';
+import { Card, Input, Button } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import Container from '../components/Container';
+import connectAlert from '../components/Alert/connectAlert';
 
 class SignIn extends Component {
   constructor(props) {
@@ -12,17 +15,101 @@ class SignIn extends Component {
       loading: false,
     };
   }
+
+  loginWithFacebook = () => {
+    return this.props.alertWithType('success', 'Error', 'Facebook log in not implemented yet');
+  }
+
+  loginWithGoogle = () => {
+    return this.props.alertWithType('info', 'Error', 'Google log in not implemented yet');
+  }
+
+  signIn = () => {
+    const { emailOrUsername, password, loading } = this.state;
+
+    if (emailOrUsername.length === 0) {
+      return this.props.alertWithType('error', 'Error', 'Email or username is required');
+    }
+
+    if (password.length === 0) {
+      return this.props.alertWithType('error', 'Error', 'Password is required');
+    }
+
+    this.setState({ loading: !loading });
+    return this.props.alertWithType('warn', 'Warning', 'Sign in not implemented yet');
+  }
+
+  /*
+    Style this correctly at some point!
+    Style google and fb sign in buttons to comply to the design guides
+    Icons are not aligned correctly. They should be aligned to left
+    on the same line as the label and the input line
+  */
   render() {
     return (
       <Container scroll>
         <Card>
           <Input
             label="Email or Username"
+            placeholder="Enter email or username"
+            keyboardType="email-address"
+            onChangeText={(emailOrUsername) => this.setState({ emailOrUsername })}
+            value={this.state.emailOrUsername}
+            leftIcon={
+              <Icon
+                name="envelope"
+                size={20}
+              />
+            }
           />
+          <Input
+            label="Password"
+            placeholder="Enter password"
+            secureTextEntry
+            onChangeText={(password) => this.setState({ password })}
+            value={this.state.password}
+            leftIcon={
+              <Icon
+                name="lock"
+                size={20}
+              />
+            }
+          />
+          <Button
+            title="Sign In"
+            iconRight
+            icon={
+              <Icon
+                name="arrow-right"
+                size={20}
+                color="white"
+              />
+            }
+            onPress={this.signIn}
+            loading={this.state.loading}
+          />
+          <Icon.Button
+            name="google"
+            backgroundColor="#000000"
+            onPress={this.loginWithGoogle}
+          >
+            Sign in with Google
+          </Icon.Button>
+          <Icon.Button
+            name="facebook"
+            backgroundColor="#3b5998"
+            onPress={this.loginWithFacebook}
+          >
+            Login with Facebook
+          </Icon.Button>
         </Card>
       </Container>
     );
   }
 }
 
-export default SignIn;
+SignIn.propTypes = {
+  alertWithType: PropTypes.func,
+};
+
+export default connectAlert(SignIn);
